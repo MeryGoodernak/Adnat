b149209da621ab34dc235e01fbecaf352f142f0c
Maryam Fazlali <fazlali1202@gmail.com>
2022-07-22 19:34:26 +0100

    Implement edit and join for organisation

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :organisations, except: %i[new]
+  resources :organisations, except: %i[new] do
+    get 'join', on: :member
+    get 'leave', on: :collection
+  end
 end
