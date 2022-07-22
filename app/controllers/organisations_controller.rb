39c3797f0f3a3204c8732a124aa37ee8e696f7f7
Maryam Fazlali <fazlali1202@gmail.com>
2022-07-22 16:29:01 +0100

    Implement show page for organisation

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -1,10 +1,21 @@
 class OrganisationsController < ApplicationController
+  before_action :set_organisation, only: %i[show edit update destroy]
+
   def index
     @organisations = Organisation.all
     @organisation = Organisation.new
   end
 
+  def show
+  end
+
   def create
+    @organisation = Organisation.new(organisation_params)
+    if @organisation.save
+      redirect_to organisations_path, notice: "Organisation was successfully added."
+    else
+      render organisations_path, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -14,5 +25,15 @@ class OrganisationsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def organisation_params
+    params.require(:organisation).permit(:name, :hourly_rate)
+  end
+
+  def set_organisation
+    @organisation = Organisation.find(params[:id])
   end
 end
