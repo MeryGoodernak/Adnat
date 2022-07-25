cd1eb78810ea802a2d0b12bbc0c0bdaaae9dd91e
Maryam Fazlali <fazlali1202@gmail.com>
2022-07-25 18:19:50 +0100

    Implement destroy shift

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,5 +1,6 @@
 class ShiftsController < ApplicationController
-  before_action :set_organisation, only: %I[index create]
+  before_action :set_organisation, only: %I[index create update destroy]
+  before_action :set_shift, only: %I[update destroy]
 
   def index
     @shifts = @organisation.users.map(&:shifts).flatten
@@ -11,21 +12,29 @@ class ShiftsController < ApplicationController
     @shift.user = current_user
     @shift.organisation = @organisation
     if @shift.save
-      redirect_to organisation_shifts_path(@organisation)
+      redirect_back fallback_location: organisation_shifts_path(@organisation)
     else
       render organisation_shifts_path(@organisation), status: :unprocessable_entity
     end
   end
 
+  def update
+
+  end
+
   def destroy
     @shift.destroy
-    redirect_to organisations_path, status: :see_other, notice: "Organisation was successfully deleted."
+    redirect_to organisation_shifts_path(@organisation), status: :see_other, notice: "Shift was successfully deleted."
   end
 
   private
 
   def set_organisation
     @organisation = Organisation.find(params[:organisation_id])
+  end
+
+  def set_shift
+    @shift = Shift.find(params[:id])
   end
 
   def shift_params
